1c04a1630552a9090230e507b776a3480d1217d2
A I <candfa2660@gmail.com>
2021-06-15 13:16:11 +0200

    Update efficiency histogram

--- scatterplot/efficiency.C
+++ scatterplot/efficiency.C
@@ -6,7 +6,9 @@
 #include <TLegend.h>
 #include <TGaxis.h>
 
-void plot(){
+void efficiency(){
+    gStyle->SetOptStat(0);
+
     TFile* delphes_file = new TFile("delphes.root");
     TH1F* truth_0       = delphes_file->Get<TH1F>("CTStarGen0");
     TH1F* truth_R       = delphes_file->Get<TH1F>("CTStarGenR");
@@ -42,7 +44,7 @@ void plot(){
     costheta_0->Divide(truth_0);
     costheta_L->Divide(truth_L);
     costheta_R->Divide(truth_R);
-    costheta_sum->Divide(truth_sum);
+    //costheta_sum->Divide(truth_sum);
     //ctstar_sum->Divide(ctstar_sum_truth_after_cuts);
 
     TCanvas *c0 = new TCanvas("costheta0old", "c0", 80, 80, 1400, 500);
@@ -58,16 +60,26 @@ void plot(){
 
     TCanvas *c1 = new TCanvas("costheta0_efficiency", "c1", 80, 80, 700, 500);
     c1->SetTitle("Delphes Efficiency");
-    ctstar_sum_truth_after_cuts->Divide(truth_sum_new);
-    ctstar_sum_truth_after_cuts->Draw("e");
+    auto a = (TH1F*)ctstar_sum_truth_after_cuts->Clone("a");
+    a->Divide(truth_sum_new);
+    a->SetTitle("Efficiency of Cuts Per Bin");
+    a->GetXaxis()->SetTitle("cos #theta*");
+    a->GetYaxis()->SetTitle("Efficiency of Cuts");
+
+    // axis titles
+    a->Draw("e");
+    a->ShowBackground()->SetLineColor(kBlue);
     // ctstar_sum->Draw("e");
 
     c1->SaveAs("../figures/Efficiency.png");
 
     TCanvas *c2 = new TCanvas("costheta0", "c2", 80, 80, 700, 500);
-    c1->SetTitle("Delphes ");
-    ctstar_sum->Draw("e");
-    ctstar_sum_truth_after_cuts->Draw("SAME e");
+    c2->Divide(2);
+    // TITLES
+    // Axis titles
+    c2->SetTitle("Delphes ");
+    c2->cd(1); ctstar_sum->Draw("e");
+    c2->cd(2); ctstar_sum_truth_after_cuts->Draw("e");
 
     //c1->SaveAs("../figures/Efficiency.png");
 
