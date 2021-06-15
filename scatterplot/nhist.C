ba277473c4557e218040b5aa4e302b2f18c52303
Julius Foverskov <julius.foverskov1@gmail.com>
2021-06-15 18:35:44 +0200

    nhist

--- scatterplot/nhist.C
+++ scatterplot/nhist.C
@@ -25,9 +25,9 @@ void nhist() {
 
     float edges[bin_count + 1] = {-1, zplus, zminus, 1};
 
-    TH1F* hist = new TH1F("a", "N distribution", bin_count, edges);
-    TH1F* hist_expectation = new TH1F("b", "2nd dist", bin_count, edges);
-    TH1F* hist_correction = new TH1F("c", "cos #theta*", bin_count, edges);
+    TH1F* hist = new TH1F("hist", "N distribution", bin_count, edges);
+    TH1F* hist_expectation = new TH1F("hist_expectation", "2nd dist", bin_count, edges);
+    TH1F* hist_correction = new TH1F("hist_correction", "cos #theta*", bin_count, edges);
 
 
     //TF1* func1 = new TF1("costheta dist","124373.6816529382*((3*0.311*(1 - x)*(1 - x))/8 + (3*0.687*(1 - x*x))/4 + (3*0.0017*(1 + x)*(1 + x))/8)",-1,1);
@@ -65,16 +65,25 @@ void nhist() {
     hist_expectation->SetLineStyle(2);
     hist_expectation->SetLineWidth(2);
     hist_correction->SetMarkerColor(kRed);
+    hist_correction->SetLineColor(kRed);
     hist_correction->SetMarkerStyle(8);    
     hist->SetMarkerStyle(8);
     hist->SetMarkerColor(kBlue);
+    hist->SetLineColor(kBlue);
+    hist_correction->SetBinError(1,401.4806632);
+    hist_correction->SetBinError(3,458.8026947);
+    hist_expectation->SetBinError(1,228.7668749);
+    hist_expectation->SetBinError(2,6.265269348e-7);
+    hist_expectation->SetBinError(3,32.74223966);
     
     TCanvas* canvas1  = new TCanvas("canvas1", "cfit", 80, 80, 1200, 700);
         
     hist_correction->Draw("E1");
     hist->Draw("SAME E1");
-    hist_expectation->Draw("SAME");
-    //func1->Draw("SAME");
+    hist_expectation->Draw("SAME E1");
+    hist_expectation->ShowBackground()->SetLineColor(kGreen);
+
+    
     legend->Draw();
   
     canvas1->SaveAs("../figures/nhist.pdf");
